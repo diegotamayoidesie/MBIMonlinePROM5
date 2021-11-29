b413c3ca02dfb45726d0998428b044c1cf279d0b
diegotamayoidesie <94146268+diegotamayoidesie@users.noreply.github.com>
2021-11-30 00:40:01 +0100

    Update 1.2_Selects-con-funciones_GrupoDTA.sql

--- 1 Select simples/1.2_Selects-con-funciones_GrupoDTA.sql
+++ 1 Select simples/1.2_Selects-con-funciones_GrupoDTA.sql
@@ -12,34 +12,53 @@ la palabra "de", el mes en minúscula en palabras, la palabra "de", el año en c
 finalizando con un punto. Luego la hora en formato 24h con minutos y segundos.
 Y de etiqueta del campo "Fecha actual".
 */
-
+SELECT TO_CHAR( SYSDATE, 'Day, DD "de" month"de" YYYY. HH24:MI:SS' ) FROM dual ;
 /* 2
 Día en palabras en el cual naciste
 */
-
+SELECT sysdate,TO_CHAR(TIMESTAMP'1982-12-01 10:00:00 -8:00', 'Day, DD "de" month"de" YYYY.') FROM DUAL;
 /* 3
 La suma de salarios, cuál es el mínimo, el máximo y la media de salario
 */
-
+select
+    sum(salary) SumaSalario,
+    min(salary) SalarioMin,
+    max(salary) SalarioMax,
+    round(avg(salary),2) SalarioMed
+from employees;
 /* 4
 Cuántos empleados hay, cuántos tienen salario y cuántos tienen comisión.
 */
-
+select
+    COUNT(*) EmpleadosTotal, 
+    count(commission_pct) EmpleadosComision,
+    count(salary) EmpleadosSalario
+from employees;
 /* 5
 Por un lado la media entre la media de salarios y el mínimo salario
 Y por otro lado, la media entre la media de salarios y el máximo salario
 Solo la parte entera, sin decimales ni redondeo.
 */
+select
+    round(((avg(salary)+min(salary))/2),0), 
+    round(((avg(salary)+max(salary))/2),0) 
+from employees;
 
 /* 6
 Listar el número de departamento y el máximo salario en cada uno de ellos.
 */
-
+select distinct
+     department_id,
+    (select max(salary)
+    from employees interna
+    where interna.department_id = externa.department_id )
+from employees externa
+order by 1 desc;
 /* 7
 Mostrar los nombres de los empleados que se repiten indicando cuántos hay del mismo
 en orden descendente.
 */
-
+select first_name,Count(first_name) from employees group by first_name having Count(first_name) >1;
 /* 8
 Mostrar en una fila cuántos empleados son jefes de departamento
 y en otra fila cuántos son jefes de otros empleados.
